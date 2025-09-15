f1236baed0b721ac6f58c80f655f6f7c76cd9c2a
RahulGadhaveDev <rahulgadhave022@gmail.com>
2025-09-15 23:44:16 +0530

    Update UserDetail.java

--- src/main/java/com/booking/entity/UserDetail.java
+++ src/main/java/com/booking/entity/UserDetail.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+//entity class 
 @Entity
 @Table(name = "bookings")
 public class UserDetail {
